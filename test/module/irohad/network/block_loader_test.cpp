17cbcc0cdf8063df9aea052769150aad86b1141d
Kitsu <mail@kitsu.me>
2018-07-06 00:19:54 +0300

    Add signature in tests for BlockLoader
    Signed-off-by: Kitsu <mail@kitsu.me>

--- test/module/irohad/network/block_loader_test.cpp
+++ test/module/irohad/network/block_loader_test.cpp
@@ -86,7 +86,8 @@ class BlockLoaderTest : public testing::Test {
     return shared_model::proto::TemplateBlockBuilder<
                kTotal,
                shared_model::validation::DefaultBlockValidator,
-               shared_model::proto::Block>()
+               shared_model::proto::UnsignedWrapper<
+                   shared_model::proto::Block>>()
         .height(1)
         .prevHash(Hash(std::string(
             shared_model::crypto::DefaultCryptoAlgorithmType::kHashLength,
@@ -97,6 +98,7 @@ class BlockLoaderTest : public testing::Test {
   std::shared_ptr<shared_model::interface::Peer> peer;
   PublicKey peer_key =
       DefaultCryptoAlgorithmType::generateKeypair().publicKey();
+  Keypair key = DefaultCryptoAlgorithmType::generateKeypair();
   std::shared_ptr<MockPeerQuery> peer_query;
   std::shared_ptr<MockBlockQuery> storage;
   std::shared_ptr<BlockLoaderImpl> loader;
@@ -111,7 +113,7 @@ class BlockLoaderTest : public testing::Test {
  */
 TEST_F(BlockLoaderTest, ValidWhenSameTopBlock) {
   // Current block height 1 => Other block height 1 => no blocks received
-  auto block = getBaseBlockBuilder().build();
+  auto block = getBaseBlockBuilder().build().signAndAddSignature(key).finish();
 
   EXPECT_CALL(*peer_query, getLedgerPeers())
       .WillOnce(Return(std::vector<wPeer>{peer}));
@@ -133,9 +135,13 @@ TEST_F(BlockLoaderTest, ValidWhenSameTopBlock) {
  */
 TEST_F(BlockLoaderTest, ValidWhenOneBlock) {
   // Current block height 1 => Other block height 2 => one block received
-  auto block = getBaseBlockBuilder().build();
+  auto block = getBaseBlockBuilder().build().signAndAddSignature(key).finish();
 
-  auto top_block = getBaseBlockBuilder().height(block.height() + 1).build();
+  auto top_block = getBaseBlockBuilder()
+                       .height(block.height() + 1)
+                       .build()
+                       .signAndAddSignature(key)
+                       .finish();
 
   EXPECT_CALL(*peer_query, getLedgerPeers())
       .WillOnce(Return(std::vector<wPeer>{peer}));
@@ -159,14 +165,18 @@ TEST_F(BlockLoaderTest, ValidWhenOneBlock) {
  */
 TEST_F(BlockLoaderTest, ValidWhenMultipleBlocks) {
   // Current block height 1 => Other block height n => n-1 blocks received
-  auto block = getBaseBlockBuilder().build();
+  auto block = getBaseBlockBuilder().build().signAndAddSignature(key).finish();
 
   auto num_blocks = 2;
   auto next_height = block.height() + 1;
 
   std::vector<wBlock> blocks;
   for (auto i = next_height; i < next_height + num_blocks; ++i) {
-    auto blk = getBaseBlockBuilder().height(i).build();
+    auto blk = getBaseBlockBuilder()
+                   .height(i)
+                   .build()
+                   .signAndAddSignature(key)
+                   .finish();
     blocks.emplace_back(clone(blk));
   }
 
@@ -192,7 +202,8 @@ TEST_F(BlockLoaderTest, ValidWhenMultipleBlocks) {
  */
 TEST_F(BlockLoaderTest, ValidWhenBlockPresent) {
   // Request existing block => success
-  auto requested = getBaseBlockBuilder().build();
+  auto requested =
+      getBaseBlockBuilder().build().signAndAddSignature(key).finish();
 
   EXPECT_CALL(*peer_query, getLedgerPeers())
       .WillOnce(Return(std::vector<wPeer>{peer}));
@@ -212,7 +223,8 @@ TEST_F(BlockLoaderTest, ValidWhenBlockPresent) {
  */
 TEST_F(BlockLoaderTest, ValidWhenBlockMissing) {
   // Request nonexisting block => failure
-  auto present = getBaseBlockBuilder().build();
+  auto present =
+      getBaseBlockBuilder().build().signAndAddSignature(key).finish();
 
   EXPECT_CALL(*peer_query, getLedgerPeers())
       .WillOnce(Return(std::vector<wPeer>{peer}));
